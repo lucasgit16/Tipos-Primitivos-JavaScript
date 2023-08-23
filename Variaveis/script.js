86e0f867d9bc9dffff00f4e05f712845e42bcebf
alexander-vtrina <alexander@vtrina.com>
2023-08-23 15:31:00 -0300

    Variaveis

---
+++ Variaveis/script.js
@@ -0,0 +1,34 @@
+// var
+
+//var alt = 5;
+//var comp = 7;
+
+//area = alt * comp;
+//console.log(area);
+//var area;
+
+//let forma = "retângulo";
+//let alt = 5;
+//let comp = 7;
+//let area;
+
+//if (forma === "retângulo"){ 
+//   area = alt * comp;
+//}   else{
+//    area = (alt * comp) / 2;   
+//}
+
+//console.log(area);
+
+//const forma = "triangulo";
+//const alt = 5;
+//const comp = 7;
+//let area;
+
+//if (forma === "quadrado") {
+//    area = alt * comp;
+//}   else{
+   // area = (alt * comp) / 2;   
+//}
+
+//console.log(area);
